a0bbdca72c1db73c5d3962146df74de7ce9a377c
Javier <javieraguileramundarain@gmail.com>
2022-08-14 19:07:49 -0300

    cambios

--- js/cards.js
+++ js/cards.js
@@ -1,87 +1,109 @@
-    //creacion de constructor
-class Ficha {
-    constructor(titulo, imagen, descripcion, precio) {
-        this.titulo = titulo;
-        this.imagen = imagen;
-        this.descripcion = descripcion;
-        this.precio = precio;
+let stockProductos = [
+    {id: 1, nombre: "Poción de amor", imagen: "../js/contenido/amor.png", desc: "Sirve para inducir amor infinito a la persona que la consuma", cantidad: 1, precio: 80},
+    {id: 2, nombre: "Poción de clarividencia", imagen: "../js/contenido/clarividencia.png", desc: "Sirve para predecir el futuro por una semana", cantidad: 1, precio: 150},
+    {id: 3, nombre: "Poción de Muerte instantanea", imagen: "../js/contenido/muerte.png", desc: "Induce la muerte de una forma discreta y sin dejar evidencia; Nota: nuestro pocion de resurrecion sirve si se aplica esta poción", cantidad: 1, precio: 500},
+    {id: 4, nombre: "Poción de Resurreción", imagen: "../js/contenido/resurrecion.png", desc: "Regresa de la muerte a cualquier persona despues de morir; Nota: maximo 3 dias para inducir desde la muerte del individuo", cantidad: 1, precio: 900},
+]
+                //importante en html
+const contenedorProductos = document.getElementById ('contenido-fichas')
+
+const contenedorCarrito = document.getElementById ('carrito-contenedor')
+
+const botonVaciar = document.getElementById ('vaciar-carrito')
+
+const contadorCarrito = document.getElementById ('contadorCarrito')
+
+const precioTotal = document.getElementById ('precioTotal')
+
+                //importante en html
+
+                //modal
+  function openmodal(selector){
+    const el = document.querySelector(selector);
+    el.classList.add("modal-opened");
+}
+
+function closemodal(selector){
+    const el = document.querySelector(selector);
+    el.classList.remove("modal-opened");
+}
+                //modal
+
+let carrito = []
+
+    document.addEventListener ("DOMContentLoaded", () => {
+        if (sessionStorage.getItem("carrito")){
+            carrito = JSON.parse(sessionStorage.getItem ("carrito"))
+            actualizarCarrito ()
         }
-    }
-                            
-const ficha1 = new Ficha ("Poción de amor", "../js/contenido/amor.png", "Sirve para inducir amor infinito a la persona que la consuma", "80$")
-const ficha2 = new Ficha ("Poción de clarividencia", "../js/contenido/clarividencia.png", "Sirve para predecir el futuro por una semana", "150$")
-const ficha3 = new Ficha ("Poción de Muerte instantanea", "../js/contenido/muerte.png", "Induce la muerte de una forma discreta y sin dejar evidencia; Nota: nuestro pocion de resurrecion sirve si se aplica esta poción", "500$")
-const ficha4 = new Ficha ("Poción de Resurreción", "../js/contenido/resurrecion.png", "Regresa de la muerte a cualquier persona despues de morir; Nota: maximo 3 dias para inducir desde la muerte del individuo", "900$")
-                                 //creacion de constructor
-                            
-                            //aplicacion del constructor
-const fichas = []                             
-                            
-fichas.push(ficha1);
-fichas.push(ficha2);
-fichas.push(ficha3);
-fichas.push(ficha4);
-                            
-                            //aplicacion del constructor
-                            
-                            //funcion reaccion al boton
-                            
-function mostrarDescripcion(ficha) {
-const contenidoFichas = document.getElementById ("contenido-fichas");
-contenidoFichas.innerHTML = "";
-                            
-contenidoFichas.innerHTML = `
-    <img src="${ficha.imagen}" alt="${ficha.titulo}">
-    <h2>${ficha.titulo}</h2>
-    <p>${ficha.descripcion}</p>
-    <h3>${ficha.precio}</h3>
-    <button id:add> comprar </button>
-    `;
-}
-                        //funcion reaccion al boton
-                            
-                         //funcion reaccion al boton regresar
-                                                        
-function crearBotonRegresar(){
-    const botonRegresar = document.createElement ("button");
-    botonRegresar.classList.add ("boton-regresar");
-    botonRegresar.innerText = "Atras";
-    botonRegresar.addEventListener("click", () => {
-        impFichas(fichas);
     })
-    document.getElementById("contenido-fichas").prepend(botonRegresar);
-}
-                        //funcion reaccion al boton regresar
-                            
-                            
-                        //Insercion del Dom y eventos
-function impFichas(fichas) {
-                            
-    const contenidoFichas = document.getElementById ("contenido-fichas");
-    contenidoFichas.innerHTML = "";
-                            
-    fichas.forEach(ficha => {
-    const divFicha = document.createElement ("div");
-    divFicha.classList.add("ficha");
-    divFicha.innerHTML = `
-    <img src="${ficha.imagen}" alt="${ficha.titulo}">
-    <h2>${ficha.titulo}</h2>
-    <h3>${ficha.precio}</h3>
-    `;
-                            //Aplicacion de Eventos
-    const botonVerDescripcion = document.createElement ("button");
-    botonVerDescripcion.innerText = "Ver Descripción";
-    botonVerDescripcion.addEventListener ("click", () =>  {
-    mostrarDescripcion(ficha);
-    crearBotonRegresar ();
+
+botonVaciar.addEventListener ("click", () =>{
+    carrito.length = 0
+    actualizarCarrito ()
+})
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement ('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src="${producto.imagen}" alt="">
+    <h2>${producto.nombre}</h2>
+    <p>${producto.desc}</p>
+    <p class="precioProducto">Precio: ${producto.precio}</p>
+    <button id= "agregar${producto.id}" class="boton-agregar">Agregar</button>
+    `
+    contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id)
     })
-    divFicha.appendChild(botonVerDescripcion)
-                            
-                    //Aplicacion de Eventos
-                                                        
-    contenidoFichas.appendChild(divFicha);
-    });
+
+})
+
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some (prod => prod.id === prodId)
+
+    if (existe) {
+        const prod = carrito.map (prod => {
+            if (prod.id === prodId){
+                prod.cantidad++
+            }
+        })
+    } else {
+
+    const item = stockProductos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    console.log(carrito);
 }
-            //Insercion del Dom y eventos
-                            
-impFichas(fichas);
+    actualizarCarrito ()
+}
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find ((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice (indice, 1)
+    actualizarCarrito ()
+}
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((prod) => {
+    const div = document.createElement('div')
+    div.innerHTML =`
+    <p>${prod.nombre}</p>
+    <p>Precio: ${prod.precio}</p>
+    <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+    <button onclick= "eliminarDelCarrito(${prod.id})"></button>
+    `
+    contenedorCarrito.appendChild(div)
+
+    sessionStorage.setItem ("carrito", JSON.stringify(carrito))
+    })
+
+    contadorCarrito.innerText = carrito.length
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+}
